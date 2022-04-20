6725944405175fc966d15e56ad76d9da63508ca4
Nochilas <patrononicholas@gmail.com>
2022-04-20 14:33:53 +0200

    Add project files.

---
+++ CoursesApp/Data/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursesApp.Data
+{
+    public class Course
+    {
+        public string ReferenceID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }       
+        public string Image { get; set; }
+    }
+}
